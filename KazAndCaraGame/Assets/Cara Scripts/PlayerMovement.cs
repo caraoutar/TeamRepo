36ce8c18120afa46b2da402ea1df6077060ef4d2
Cara Outar <co884@nyu.edu>
2021-04-11 17:27:59 -0400

    moved objects around in my scene

--- KazAndCaraGame/Assets/Cara Scripts/PlayerMovement.cs
+++ KazAndCaraGame/Assets/Cara Scripts/PlayerMovement.cs
@@ -21,7 +21,10 @@ private Vector3 nextPos;
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)) {
+
+            Debug.Log("pressed mouse"); 
+        }
     }
 
     void FixedUpdate() {
