f23be9d18978e3201cec423baad723044b466d73
Cara Outar <co884@nyu.edu>
2021-04-11 17:01:08 -0400

    Began player movement script

--- KazAndCaraGame/Assets/Cara Scripts/PlayerMovement.cs
+++ KazAndCaraGame/Assets/Cara Scripts/PlayerMovement.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+//movement vars
+
+public float speed;
+
+private Vector3 nextPos;
    
 
     // Start is called before the first frame update
